cea45c5bc402b5efd442a105f90ba047ad0250bb
Liam Selfors <lselfors@gmail.com>
2022-10-25 07:00:42 -0500

    Initialized scraping.py

--- scraping.py
+++ scraping.py
@@ -7,6 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def scrape_all():
+    
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
